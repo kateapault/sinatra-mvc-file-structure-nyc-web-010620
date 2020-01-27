90ce4f8b978fff59ffa4e6ea7ca5ae2519126076
Kate Raskauskas <katherine.raskauskas@gmail.com>
2020-01-27 18:49:59 -0500

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,17 @@
-class Model
+class Model < Sinatra::Base
+    attr_accessor :name, :breed, :age
+    @@all = []
+
+    def initialize(name,breed,age)
+        @name = name
+        @breed = breed
+        @age = age
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
